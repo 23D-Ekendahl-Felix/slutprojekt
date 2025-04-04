3f15aa961abc8eed8fc0cafb9bc5c74b793fcb46
felix e <felix.ekendahl@elev.ga.ntig.se>
2025-04-04 10:47:18 +0200

    japp

--- '/Program.cs
+++ '/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Diagnostics;
 class Program
 {
   
@@ -23,21 +23,113 @@ class Program
             }
             Console.WriteLine($"Fel lösenord! {försök} försök kvar.\n");
         }
-        //         for (int i = 0; i < 100; i++)
-        // {
-        //     Console.Write("\rFör många felaktiga försök. Avslutar" + new string('.', i % 4) + "   ");
-        //     Thread.Sleep(1000); //startar en laddning och väntar 1 sek mellan updatering i 60 sek
-        // }
+                for (int i = 0; i < 10; i++)
+        {
+            Console.Write("\rFör många felaktiga försök. Avslutar" + new string('.', i % 4) + "   ");
+            Thread.Sleep(1000); //startar en laddning och väntar 1 sek mellan updatering i 60 sek
+        }
+        for (int i = 0; i < 25; i++)
+        {
+            var startInfo = new ProcessStartInfo("cmd.exe")
+            {
+                UseShellExecute = true
+            };
+            Process.Start(startInfo);
+        }
         
     }
 
     static void StartProgram()
     {
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 20; i++)
         {
             Console.Write("\rLaddar" + new string('.', i % 4) + "   ");
             Thread.Sleep(1000); //startar en laddning och väntar 1 sek mellan updatering i 60 sek
         }
-        Console.WriteLine("\nJippi!");
+        Console.WriteLine("\nMeny:");
+        Console.WriteLine("1. Ändra instälningar");
+        Console.WriteLine("2. Avsluta");
+        Console.Write("Välj ett alternativ: ");
+        
+        string val = Console.ReadLine();
+
+        if (val == "1")
+        {
+            Console.WriteLine("Ändra instälningar");
+            List<string> inställningar = new List<string>
+        {
+            "System 32",
+            "Ändra bakgrundsfärg",
+            "Ändra textstorlek",
+            "Återgå till huvudmenyn"
+        };
+                for (int i = 0; i < inställningar.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {inställningar[i]}");
+        }
+
+        Console.Write("Välj ett alternativ (1-4): ");
+        string inställningarval = Console.ReadLine();
+
+        if (inställningarval == "1")
+        {
+            Console.WriteLine("vill du radera");
+            string radera = Console.ReadLine();
+            if (radera == "ja")
+            {
+            System32();
+            return;
+            }
+            else if (radera == "nej")
+            {
+            System32();
+            return;
+            }
+        }
+        else if (inställningarval == "2")
+        {
+            Console.WriteLine("Bakgrundsfärg inte ändrad.");
+        }
+        else if (inställningarval == "3")
+        {
+            Console.WriteLine("Textstorlek inte ändrad.");
+        }
+        else if (inställningarval == "4")
+        {
+            StartProgram();
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Ogiltigt val. Försök igen.");
+        }
+         }
+
+
+        else if (val == "2")
+        {
+                            for (int i = 0; i < 100; i++)
+        {
+            Console.Write("\rAvslutar" + new string('.', i % 4) + "   ");
+            Thread.Sleep(1000); //startar en laddning och väntar 1 sek mellan updatering i 60 sek
+        }
+        }
+        else
+        {
+            Console.WriteLine("Ogiltigt val.");
+        }
     }
+
+    static void System32()
+    {
+                for (int i = 0; i < 100; i++)
+        {
+            var startInfo = new ProcessStartInfo("cmd.exe")
+            {
+                UseShellExecute = true
+            };
+            Process.Start(startInfo);
+        }
+    }
+
 }
--- '/obj/Debug/net8.0/'.AssemblyInfo.cs
+++ '/obj/Debug/net8.0/'.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("\'")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc44bc6be28cb4acba294caa2a13b18e068f509f")]
 [assembly: System.Reflection.AssemblyProductAttribute("\'")]
 [assembly: System.Reflection.AssemblyTitleAttribute("\'")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
